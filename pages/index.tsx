666c01cc6306854e7c5cad06dda671b4c00f6ece
Praise Obende <davidobende@gmail.com>
2022-06-28 18:43:43 +0100

    New changes added

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,9 +36,13 @@ const Home: NextPage = () => {
       setIsCorrect(false);
       setColor("red");
     }
+    setTimeout(() => {
+      setNumber((prev) => prev + 1);
+      setIsShow(false);
+    }, 2000);
   };
 
-  const fetchQuiz = async () => {
+  const fetchQuiz = () => {
     const options = {
       method: "GET",
       url: "https://twinword-word-association-quiz.p.rapidapi.com/type1/",
@@ -52,8 +56,13 @@ const Home: NextPage = () => {
     axios
       .request(options)
       .then((response) => {
-        setQuizList(response.data.quizlist);
-        setIsLoading(false);
+        setTimeout(() => {
+          setQuizList(response.data.quizlist);
+        }, 300);
+
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1300);
       })
       .catch((error) => {
         console.error(error);
@@ -61,10 +70,8 @@ const Home: NextPage = () => {
   };
 
   useEffect(() => {
-    fetchQuiz();
-    console.log(quizList);
-    console.log(number);
     // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchQuiz();
   }, []);
   return (
     <div className={styles.container}>
@@ -77,84 +84,89 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <section>
-          <div className={styles.topWrapper}>
-            <h2 className={styles.qnumber}>
-              Question {number + 1} of {quizList.length}
-            </h2>
-            {score !== 0 ? (
-              <h3 className={styles.scores}>Score: {score} </h3>
-            ) : null}
-          </div>
+        {!isLoading ? (
+          <section>
+            <section>
+              <div className={styles.topWrapper}>
+                <h2 className={styles.qnumber}>
+                  Question {number + 1} of {quizList.length}
+                </h2>
+                {score !== 0 ? (
+                  <h3 className={styles.scores}>Score: {score} </h3>
+                ) : null}
+              </div>
 
-          <hr />
-          <h4 className={styles.question}>
-            Which of the options relates to the following words?
-          </h4>
+              <hr />
+              <h4 className={styles.question}>
+                Which of the options relates to the following words?
+              </h4>
+              {!isLoading ? (
+                quizList[number].quiz.map(
+                  (
+                    item:
+                      | boolean
+                      | Key
+                      | ReactElement<any, string | JSXElementConstructor<any>>
+                      | ReactFragment
+                      | null
+                      | undefined
+                  ) => {
+                    return (
+                      <span key={item} className={styles.words}>
+                        {item}
+                      </span>
+                    );
+                  }
+                )
+              ) : (
+                <h2>Loading...</h2>
+              )}
 
-          {!isLoading ? (
-            quizList[number].quiz.map(
-              (
-                item:
-                  | boolean
-                  | Key
-                  | ReactElement<any, string | JSXElementConstructor<any>>
-                  | ReactFragment
-                  | null
-                  | undefined
-              ) => {
-                return (
-                  <span key={item} className={styles.words}>
-                    {item}
-                  </span>
-                );
-              }
-            )
-          ) : (
-            <h2>Loading...</h2>
-          )}
+              {!isLoading
+                ? quizList[number].option.map(
+                    (
+                      item:
+                        | boolean
+                        | Key
+                        | ReactFragment
+                        | ReactElement<any, string | JSXElementConstructor<any>>
+                        | null
+                        | undefined,
+                      index: any
+                    ) => {
+                      return (
+                        <span
+                          className={styles.option}
+                          key={item}
+                          onClick={() =>
+                            handleAnswer(index, quizList[number].correct)
+                          }
+                        >
+                          {item}
+                        </span>
+                      );
+                    }
+                  )
+                : null}
+            </section>
 
-          {!isLoading
-            ? quizList[number].option.map(
-                (
-                  item:
-                    | boolean
-                    | Key
-                    | ReactFragment
-                    | ReactElement<any, string | JSXElementConstructor<any>>
-                    | null
-                    | undefined,
-                  index: any
-                ) => {
-                  return (
-                    <span
-                      className={styles.option}
-                      key={item}
-                      onClick={() =>
-                        handleAnswer(index, quizList[number].correct)
-                      }
-                    >
-                      {item}
-                    </span>
-                  );
-                }
+            {isShow ? (
+              isCorrect ? (
+                <p className={styles.correct}>Correct!</p>
+              ) : (
+                <p className={styles.wrong}>Wrong!</p>
               )
-            : null}
-        </section>
+            ) : null}
 
-        {isShow ? (
-          isCorrect ? (
-            <p className={styles.correct}>Correct!</p>
-          ) : (
-            <p className={styles.correct}>Wrong!</p>
-          )
-        ) : null}
-
-        {number + 1 !== 10 ? (
-          <button className={styles.button} onClick={handleNext}>
-            Next
-          </button>
-        ) : null}
+            {number + 1 !== 10 ? (
+              <button className={styles.button} onClick={handleNext}>
+                Next
+              </button>
+            ) : null}
+          </section>
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </main>
 
       <footer className={styles.footer}>
